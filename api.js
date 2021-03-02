355ead0d357a14b8b138a9630ce5d577f55e957c
min-elena-k <dev.elena.k@gmail.com>
2021-03-02 23:50:03 +0900

    Divide and Conquer - getAnything()

--- api.js
+++ api.js
@@ -3,27 +3,38 @@ import axios from "axios";
 const TMDB_Key = "e1d27b00134d34cc50c076802bfb3006";
 
 const makeRequest = (path, params) => 
-    axios.get(`https://api.themoviedb.org/3/${path}`, {
+    axios.get(`https://api.themoviedb.org/3${path}`, {
         params: {
             ...params,
             api_key: TMDB_Key
         }
     });
 
+const getAnything = async (paht, params = {}) => {
+    try {
+        const {
+            data: { results }
+        } = await makeRequest(path, params);
+        return [results, null]
+    } catch(e) {
+            return [null, e]
+    }
+};
+
 export const movieApi = {
-    nowPlaying: () => makeRequest("/movie/now_playing"),
-    popular: () => makeRequest("/movie/popular"),
-    upcoming: () => makeRequest("/movie/upcoming", {region: "kr"}),
-    search: query => makeRequest("/search/movie", { query }),
-    movie: id => makeRequest(`movie/${id}`),
-    discover: () => makeRequest("discover/movie")
+    nowPlaying: () => getAnything("/movie/now_playing"),
+    popular: () => getAnything("/movie/popular"),
+    upcoming: () => getAnything("/movie/upcoming", {region: "kr"}),
+    search: query => getAnything("/search/movie", { query }),
+    movie: id => getAnything(`movie/${id}`),
+    discover: () => getAnything("discover/movie")
 }
 
 export const tvApi = {
-    today: () => makeRequest("/tv/airing_today"),
-    thisWeek: () => makeRequest("/tv/on_the_air"),
-    topRated: () => makeRequest("/tv/top_rated"),
-    popular: () => makeRequest("/tv/popular"),
-    search: query => makeRequest("/search/tv", { query }),
-    show: id => makeRequest(`/tv/${id}`)
+    today: () => getAnything("/tv/airing_today"),
+    thisWeek: () => getAnything("/tv/on_the_air"),
+    topRated: () => getAnything("/tv/top_rated"),
+    popular: () => getAnything("/tv/popular"),
+    search: query => getAnything("/search/tv", { query }),
+    show: id => getAnything(`/tv/${id}`)
 }
--- screens/Movies.js
+++ screens/Movies.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Button } from "react-native";
+import { movieApi } from "../api"
 
-export default ({ navigation }) => (
-    <View style={{ backgroundColor: "black", flex:1}}>
-        <Text>Movies</Text>
-        <Button title="Movie" onPress={() => navigation.navigate("Detail")} />
-    </View>
-);
+export default () => {
+    const getData = async () => {
+        const [nowPlaying, error] = await movieApi.nowPlaying();
+        const [popular, papularError] = await movieApi.popular();
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
+
+    return (
+        <View style={{ backgroundColor: "black", flex:1}}>
+            <Text>Movies</Text>
+        </View>
+    )
+};
