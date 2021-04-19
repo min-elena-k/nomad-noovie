4457d42307f3cfde4d266c60a6bbea702b5812b5
mjkim928 <mjkim@kimzipsa.co.kr>
2021-04-19 16:58:15 +0900

    Slider Background, Content

---
+++ components/Movie/Slide.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import PropTypes from 'prop-types';
+import { Dimensions } from 'react-native';
+import { apiImage } from '../../api';
+import Poster from '../Poster';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+
+const Container = styled.View`
+	width: 100%;
+	height: 100%;
+`;
+
+const BG = styled.Image`
+	width: 100%;
+	height: 100%;
+	opacity: 0.4;
+	position: absolute;
+`;
+
+const Content = styled.View`
+	height: 100%
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-around;
+`;
+
+const Data = styled.View`
+	width: 50%;
+	align-items: flex-start;
+`;
+
+const Title = styled.Text`
+	color: white;
+	font-weight: bold;
+	font-size: 19px;
+	margin-bottom: 10px;
+`;
+
+const Votes = styled.Text`
+	color: rgb(220, 220, 220);
+	margin-bottom: 7px;
+	font-size: 12px;
+	font-weight: 500;
+
+`;
+
+const Overview = styled.Text`
+	color: rgb(220, 220, 220);
+	font-size: 14px;
+	font-weight: 500;
+`;
+
+const Button = styled.View`
+	margin-top: 10px;
+	background-color: #e74c3c;
+	padding: 7px 10px;
+	border-radius: 3px;
+
+`;
+
+const ButtonText = styled.Text`
+		color: white;
+`;
+
+const Slide = ({ id, title, backgroundImage, votes, overview, poster }) => {
+	return (
+		<Container>
+			<BG source={{uri: apiImage(backgroundImage)}} />
+			<Content>
+				<Poster url={apiImage(poster)} />
+				<Data>
+					<Title>{title.length > 40 ? `${title.slice(0, 40)}...` : title}</Title>
+					<Votes>⭐ {votes} / 10</Votes>
+					<Overview>{overview.length > 120 ? `${overview.slice(0, 120)}...` : overview}</Overview>
+					<TouchableOpacity>
+						<Button>
+							<ButtonText>View Details</ButtonText>
+						</Button>
+					</TouchableOpacity>
+				</Data>
+			</Content>
+		</Container>
+	);
+};
+
+Slide.propTypes = {
+	id: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
+	backgroundImage: PropTypes.string.isRequired,
+	votes: PropTypes.number.isRequired,
+	overview: PropTypes.string.isRequired,
+	poster: PropTypes.string.isRequired
+}
+
+export default Slide;
--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -2,47 +2,40 @@ import React from 'react';
 import styled from "styled-components/native";
 import Swiper from "react-native-web-swiper";
 import { ActivityIndicator, Dimensions } from "react-native";
+import Slide from '../../components/Movie/Slide';
 
-const {width, height } = Dimensions.get("screen");
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("screen");
 
 const Container = styled.View`
-    flex: 1;
-    background-color: black;
-    justify-content: center;
+  flex: 1;
+  background-color: black;
+  justify-content: center;
 `;
 
-const Header = styled.View`
-    width: 100%;
-    height: ${height / 3}px;
+const SliderContainer = styled.View`
+  width: ${WIDTH}px;
+	height: ${HEIGHT / 4  }px;
 `;
 
-const Section = styled.View`
-    background-color: red;
-    height: 100%;
-`;
-
-const Text = styled.Text``;
-
-const MoviesPresenter = ({ loading, nowPlaying }) => {
-    console.log(nowPlaying)
-    return (
-        <Container>
-            {loading ? (
-                <ActivityIndicator color="white" size="small"/>
-            ) : (
-            <Header>
-                <Swiper controlsEnabled={false} loop timeout={3}>
-                   {nowPlaying.map(movie => (
-                    //    <Text key={movie.id}> di~</Text>
-                        <Section key={movie.id}>
-                            <Text>{movie.original_title}</Text>
-                        </Section>
-                   ))}
-                </Swiper>
-            </Header>
-            )}
-        </Container>
-    );
-};
-
-export default MoviesPresenter;
+export default ({ loading, nowPlaying }) => (
+  <Container>
+    {nowPlaying.length == 0 ? (
+      <ActivityIndicator color="white" size="small" />
+    ) : (
+      <SliderContainer>
+        <Swiper controlsEnabled={false} loop timeout={3} >
+          {nowPlaying.map(movie => (
+            <Slide 
+              key={movie.id} 
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              votes={movie.vote_average}
+              backgroundImage={movie.backdrop_path}
+              poster={movie.poster_path} />
+          ))}
+        </Swiper>
+      </SliderContainer>
+    )}
+  </Container>
+)
